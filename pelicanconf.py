64ad52c3cfb33b2d0020dceab5346c527e28be9a
Faris Priadi <farispriadi@gmail.com>
2020-08-02 21:05:54 +0700

    menambahkan theme hyde

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,14 +3,19 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Faris Priadi'
-SITENAME = 'eFPe Notes'
-SITEURL = 'https://farispriadi.github.io'
+SITENAME = 'Faris Priadi'
+SITEURL = 'https://farispriadi.github.io' #'http://localhost:8000'
+BIO = 'Love Code with Python'
+
+PROFILE_IMAGE = 'avatar1.jpeg'
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Jakarta'
 
 DEFAULT_LANG = 'en'
+
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,6 +24,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+THEME = 'pelican-themes/hyde'
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
@@ -26,10 +33,20 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/faris-priadi-9ba17245'),
+          ('github', 'https://github.com/farispriadi'),)
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# SIDEBAR_DIGEST = ''
+
+# # FAVICON = 'url-to-favicon'
+
+# DISPLAY_PAGES_ON_MENU = True
+
+# # TWITTER_USERNAME = 'twitter-user-name'
+
+# MENUITEMS = (('Blog', SITEURL),)
